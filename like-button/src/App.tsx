af6d6a405547f93744f3e3eee61139fd0e5cd274
hogucc <uku.h1r8@gmail.com>
2022-05-15 18:37:11 +0900

    いいねボタン予定地を追加

--- like-button/src/App.tsx
+++ like-button/src/App.tsx
@@ -5,22 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+            <LikeButton />
+        </header>
     </div>
   );
+}
+
+function LikeButton() {
+    return <span>いいねボタン予定地</span>;
 }
 
 export default App;
